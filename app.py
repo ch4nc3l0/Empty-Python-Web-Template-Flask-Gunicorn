73e5f084bbc702d9cb971f0c40b578f2ee1cbaf3
chance <ch4nc3l0@gmail.com>
2021-01-14 17:26:08 -0500

    Remove unneeded imports and req.

--- app.py
+++ app.py
@@ -2,17 +2,13 @@
 # Above line is to set the interpreter
 
 # Imports
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask import Flask, request, url_for, render_template, redirect, session, make_response, jsonify, flash
-import random, string, os, httplib2, json, requests, httplib2
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
-from flask import session as login_session
+from flask import Flask, request, url_for, render_template
 
 
+# App Setup
 app = Flask(__name__)
 app.static_folder = 'static'
+
 
 # Main Page
 @app.route('/')
